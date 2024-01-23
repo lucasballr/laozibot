292793efc8b1f90bd9c1b01caa80ffc372df12c3
lucas <ball.lucas@gmail.com>
2024-01-23 15:20:14 -0800

    added some logs

--- typebot/bot.ts
+++ typebot/bot.ts
@@ -1,7 +1,36 @@
 import { Client, GatewayIntentBits } from 'discord.js';
+import { REST, Routes } from 'discord.js';
 import dotenv from 'dotenv';
 
 dotenv.config();
+const token = process.env.DISCORD_TOKEN
+//const clientId = '1199477265457758239'
+
+const commands = [
+  {
+    name: 'ping',
+    description: 'Replies with Pong!',
+  },
+];
+
+const rest = new REST({ version: '10' }).setToken(token!);
+
+// (async () => {
+//     try {
+//         console.log(`Started refreshing ${commands.length} application (/) commands.`);
+
+//         // The put method is used to fully refresh all commands in the guild with the current set
+//         const data = await rest.put(
+//             Routes.applicationGuildCommands(clientId!, guildId),
+//             { body: commands },
+//         );
+
+//         console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+//     } catch (error) {
+//         // And of course, make sure you catch and log any errors!
+//         console.error(error);
+//     }
+// })();
 
 const client = new Client({ intents: [
 		GatewayIntentBits.Guilds,
@@ -11,6 +40,12 @@ const client = new Client({ intents: [
 
 client.once('ready', () => {
     console.log('Ready!');
+    console.log(`Logged in as ${client.user?.tag}!. Here is my ID: ${client.user?.id}!`)
+    console.log("Here are my guild id's");
+    let guildIds = client.guilds.fetch();
+    for (let guildId in guildIds){
+        console.log(guildId);
+    }
 });
 
 client.on('messageCreate', message => {
@@ -20,4 +55,4 @@ client.on('messageCreate', message => {
 });
 
 
-client.login(process.env.DISCORD_TOKEN);
+client.login(token!);
