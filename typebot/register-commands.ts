8b7db8d51f2d0a0bccdaa3588be6c88ac70d5580
lucas <ball.lucas@gmail.com>
2024-01-23 20:40:24 -0800

    maybe?

--- typebot/register-commands.ts
+++ typebot/register-commands.ts
@@ -16,7 +16,6 @@ const commands = [
     description: 'Plays music from YouTube',
     options: [{
       name: 'link',
-      type: 'STRING',
       description: 'The YouTube link',
       required: true,
     }],
